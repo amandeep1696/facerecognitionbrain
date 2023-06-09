9b8dda19f22af8bc16a0bbe241a7b2824478b67b
Amandeep Singh <am418470@dal.ca>
2023-06-09 00:29:02 -0300

    Connected register users to backend

--- src/App.js
+++ src/App.js
@@ -18,8 +18,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   componentDidMount() {
     fetch('http://localhost:3000')
@@ -125,7 +144,10 @@ class App extends Component {
         ) : route === 'signin' ? (
           <SignIn onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
